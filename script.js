f75f7c73cb1f5a1fc146a2a9d94c18102581731b
Isaac orija <isaac.orija@gmail.com>
2020-02-13 03:04:31 -0800

    fixed name

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var i = 0;
 var txt = `Dear Mary,
-I can't think of anyone else. I would want to have as mine...No one else gives me the tingles. The way you always do, So, will you be my valentine? So I can share this with you...On this day made just for lovers. So imma ask you again Mary Onesa Uruoghor, will you be my val?
+I can't think of anyone else. I would want to have as mine...No one else gives me the tingles. The way you always do, So, will you be my valentine? So I can share this with you...On this day made just for lovers. So imma ask you again Mary Onesa Oruoghor, will you be my val?
 
 `;
 var speed = 100;
